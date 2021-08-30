9f19194b71631f2857a9abc4e0966f4343289fd7
Priyanka Arepalli <parepalli@ad.bluemsp.com>
2021-08-30 11:27:57 +0530

    Download reports fixes

--- ngDesk-Report-Service/src/main/java/com/ngdesk/repositories/CustomCompanyRepository.java
+++ ngDesk-Report-Service/src/main/java/com/ngdesk/repositories/CustomCompanyRepository.java
@@ -1,12 +1,11 @@
 package com.ngdesk.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.ngdesk.report.company.dao.Company;
 
 public interface CustomCompanyRepository {
 
-	public Optional<List<Company>> findAllCompanies(String collectionName);
+	public List<Company> findAllCompanies(String collectionName);
 
 }
