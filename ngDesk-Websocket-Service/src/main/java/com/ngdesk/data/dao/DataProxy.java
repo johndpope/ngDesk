91b783d0ba633b24be09ee8a46f83d2e2a9b0837
dharanids <dharani.dalavayi@allbluesolutions.com>
2021-09-08 16:49:47 +0530

    Added dataproxy

---
+++ ngDesk-Websocket-Service/src/main/java/com/ngdesk/data/dao/DataProxy.java
@@ -0,0 +1,30 @@
+package com.ngdesk.data.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "ngdesk-data-service-v1")
+public interface DataProxy {
+
+	@PostMapping("/modules/{module_id}/data")
+	public Map<String, Object> postModuleEntry(@RequestBody HashMap<String, Object> entry,
+			@PathVariable("module_id") String moduleId,
+			@RequestParam(value = "is_trigger", required = false) boolean isTrigger,
+			@RequestParam(value = "company_id", required = false) String companyId,
+			@RequestParam(value = "user_uuid", required = false) String userUuid);
+
+	@PutMapping("/modules/{module_id}/data")
+	public Map<String, Object> putModuleEntry(@RequestBody HashMap<String, Object> entry,
+			@PathVariable("module_id") String moduleId,
+			@RequestParam(value = "is_trigger", required = false) boolean isTrigger,
+			@RequestParam(value = "company_id", required = false) String companyId,
+			@RequestParam(value = "user_uuid", required = false) String userUuid);
+
+}
