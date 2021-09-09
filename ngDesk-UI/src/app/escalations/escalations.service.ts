8c5b2be15d8ed76f478f5b2842bd33e6955da21c
Swaraj Mohanty <smohanty@ad.bluemsp.com>
2021-09-09 08:24:40 +0530

    147-Added code

--- ngDesk-UI/src/app/escalations/escalations.service.ts
+++ ngDesk-UI/src/app/escalations/escalations.service.ts
@@ -115,16 +115,16 @@ export class EscalationsService {
 						TEAM_IDS: teamIds
 					}
 				}
-				dateCreated
-				dateUpdated
-				createdBy {
+				DATE_CREATED: dateCreated
+				DATE_UPDATED: dateUpdated
+				CREATED_BY: createdBy {
 					DATA_ID: _id
 					CONTACT {
 						PRIMARY_DISPLAY_FIELD: FULL_NAME
 						DATA_ID: _id
 					}
 				}
-				lastUpdatedBy {
+				LAST_UPDATED_BY: lastUpdatedBy {
 					DATA_ID: _id
 					CONTACT {
 						FULL_NAME
