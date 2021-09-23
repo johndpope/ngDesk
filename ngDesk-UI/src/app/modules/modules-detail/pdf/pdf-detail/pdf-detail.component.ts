0b234aa6c65ecbadfb929c92fe6f0082931e82a5
Ayush Verma <ayush.verma@allbluesolutions.com>
2021-09-23 16:48:16 +0530

    Fixed in edit layout

--- ngDesk-UI/src/app/modules/modules-detail/pdf/pdf-detail/pdf-detail.component.ts
+++ ngDesk-UI/src/app/modules/modules-detail/pdf/pdf-detail/pdf-detail.component.ts
@@ -86,9 +86,7 @@ export class PdfDetailComponent implements OnInit, OnDestroy {
 				.subscribe(
 					(response: any) => {
 						this.isLoading = false;
-						const template = response.HTML_TEMPLATE.match(
-							/\b[^<p>.][^a-z][A-Z_]*.[A-Z_]*\b/g
-						);
+						const template = response.HTML_TEMPLATE.match(/\b{{.*?}}/g);
 						template.forEach((field, index) => {
 							if (index == template.indexOf(field)) {
 								response.HTML_TEMPLATE = response.HTML_TEMPLATE.replaceAll(
