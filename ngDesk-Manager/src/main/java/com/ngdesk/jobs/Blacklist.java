90ef872bdfe12a6bf70a5878a0d93b32970b6801
vmirji <vishal.mirji@allbluesolutions.com>
2021-10-07 13:29:59 +0530

    Update Blacklist.java
    Issue Fixed

--- ngDesk-Manager/src/main/java/com/ngdesk/jobs/Blacklist.java
+++ ngDesk-Manager/src/main/java/com/ngdesk/jobs/Blacklist.java
@@ -43,7 +43,7 @@ public class Blacklist {
 
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private RedissonClient redisson;
 
@@ -51,12 +51,12 @@ public class Blacklist {
 	public void executeJob() {
 		log.trace("Enter Blacklist.executeJob()");
 		String subdomain = "";
-		
+
 		try {
-			
+
 			RMap<String, Map<String, List<Timestamp>>> outgoingMails = redisson.getMap("outgoingMails");
 			RMap<String, Map<String, List<Timestamp>>> incomingMails = redisson.getMap("incomingMails");
-			
+
 			for (String companyId : incomingMails.keySet()) {
 				MongoCollection<Document> blacklistCollection = mongoTemplate
 						.getCollection("blacklisted_whitelisted_emails_" + companyId);
@@ -95,6 +95,9 @@ public class Blacklist {
 
 								MongoCollection<Document> usersCollection = mongoTemplate
 										.getCollection("Users_" + companyId);
+								MongoCollection<Document> contactCollection = mongoTemplate
+										.getCollection("Contacts_" + companyId);
+
 								List<Document> adminUsers = usersCollection
 										.find(Filters.and(Filters.eq("ROLE", roleId), Filters.eq("DELETED", false),
 												Filters.or(Filters.eq("EFFECTIVE_TO", null),
@@ -103,12 +106,16 @@ public class Blacklist {
 
 								String subject = "An email has been blocked";
 								for (Document user : adminUsers) {
+
+									Document contacts = (Document) contactCollection
+											.find(Filters.eq("_id", new ObjectId(user.getString("CONTACT")))).first();
+
 									String emailAddress = user.getString("EMAIL_ADDRESS");
-									String firstName = user.getString("FIRST_NAME");
+									String firstName = contacts.get("FIRST_NAME").toString();
 									String lastName = "";
 
-									if (user.containsKey("LAST_NAME") && user.get("LAST_NAME") != null) {
-										lastName = user.getString("LAST_NAME");
+									if (contacts.containsKey("LAST_NAME") && contacts.get("LAST_NAME") != null) {
+										lastName = contacts.get("LAST_NAME").toString();
 									}
 
 									String body = global.getFile("blacklist-incoming-body.html");
@@ -132,10 +139,10 @@ public class Blacklist {
 							if (timestamps.size() == 0) {
 								incomingEmailsToRemove.add(email);
 							} else {
-								
-								Map<String,List<Timestamp>> userTimestamps = incomingMails.get(companyId);
+
+								Map<String, List<Timestamp>> userTimestamps = incomingMails.get(companyId);
 								userTimestamps.put(email, timestamps);
-								
+
 								incomingMails.put(companyId, userTimestamps);
 							}
 
@@ -221,7 +228,7 @@ public class Blacklist {
 							if (timestamps.size() == 0) {
 								outgoingEmailsToRemove.add(email);
 							} else {
-								
+
 								Map<String, List<Timestamp>> userTimes = outgoingMails.get(companyId);
 								userTimes.put(email, timestamps);
 								outgoingMails.put(companyId, userTimes);
@@ -230,13 +237,13 @@ public class Blacklist {
 						}
 					}
 				}
-				
+
 				Map<String, List<Timestamp>> userTimes = incomingMails.get(companyId);
 				for (String email : incomingEmailsToRemove) {
 					userTimes.remove(email);
 				}
 				incomingMails.put(companyId, userTimes);
-				
+
 				Map<String, List<Timestamp>> userTimestamps = outgoingMails.get(companyId);
 				for (String email : outgoingEmailsToRemove) {
 					userTimestamps.remove(email);
