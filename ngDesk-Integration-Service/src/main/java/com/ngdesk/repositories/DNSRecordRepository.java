477da2ca711a5dc3e558ef88f1ccf23dc20b746b
Sharath Satish <sharath.satish@allbluesolutions.com>
2021-10-13 16:11:05 +0530

    Removed Integration Service

--- ngDesk-Integration-Service/src/main/java/com/ngdesk/repositories/DNSRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ngdesk.repositories;
-
-import java.util.Map;
-
-public interface DNSRecordRepository
-		extends CustomDNSRecordRepository, CustomNgdeskRepository<Map<String, Object>, String> {
-
-}
