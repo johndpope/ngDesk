0eee4505b1f69180e583bba7c85cc56b8801d0cb
sivachalapati <sivanarayana.chalapati@subscribeit.com>
2021-10-25 08:21:44 +0530

    23-Knowladgebase-Section,-Category-and-Articles-API-Integration
    Changes done

--- ngDesk-UI/src/app/guide/arrange/arrange-articles/arrange-articles.component.ts
+++ ngDesk-UI/src/app/guide/arrange/arrange-articles/arrange-articles.component.ts
@@ -7,112 +7,125 @@ import { Section } from '../../../models/section';
 import { GuideService } from '../../guide.service';
 
 @Component({
-  selector: 'app-arrange-articles',
-  templateUrl: './arrange-articles.component.html',
-  styleUrls: ['../../guide.component.scss']
+	selector: 'app-arrange-articles',
+	templateUrl: './arrange-articles.component.html',
+	styleUrls: ['../../guide.component.scss'],
 })
 export class ArrangeArticlesComponent implements OnInit {
+	public category: Category = new Category('', '', false, 0);
+	public section: Section = new Section('', '', '', '', 0);
+	public articles = [];
+	public sectionId: string;
+	public errorMessage: string;
+	public isLoading = true;
 
-  public category: Category = new Category('', '', false, 0);
-  public section: Section = new Section('', '', '', '', 0);
-  public articles = [];
-  public sectionId: string;
-  public errorMessage: string;
-  public isLoading = true;
+	constructor(
+		private route: ActivatedRoute,
+		private guideService: GuideService
+	) {
+		this.sectionId = this.route.snapshot.params['sectionId'];
+		this.guideService.getkbSections(this.sectionId).subscribe(
+			(sectionsResponse: any) => {
+				console.log('sectionsResponse', sectionsResponse);
 
-  constructor(
-    private route: ActivatedRoute,
-    private guideService: GuideService,
-  ) {
-    this.sectionId = this.route.snapshot.params['sectionId'];
-    this.guideService.getSectionById(this.sectionId).subscribe(
-      (sectionsResponse: any) => {
-        this.section = this.convertSection(sectionsResponse);
-        this.guideService.getCategoryById(this.section.category).subscribe(
-          (categoryResponse: any) => {
-            this.category = this.convertCategory(categoryResponse);
-            this.guideService.getArticlesBySection(this.section.sectionId, false).subscribe(
-              (articlesResponse: any) => {
-                this.articles = articlesResponse.DATA.filter(article => article.SECTION === this.section.sectionId);
-                this.articles.sort((n1, n2) => {
-                  if (n1.ORDER > n2.ORDER) {
-                    return 1;
-                  }
+				this.section = this.convertSection(sectionsResponse);
+				this.guideService
+					.getKbSectionByCategoryId(this.category.categoryId)
+					.subscribe(
+						(categoryResponse: any) => {
+							console.log('categoryResponse', categoryResponse);
+							this.category = this.convertCategory(categoryResponse);
+							this.guideService
+								.getArticlesBySectionId(this.section.sectionId)
+								.subscribe(
+									(articlesResponse: any) => {
+										console.log('articlesResponse', articlesResponse);
+										this.articles =
+											articlesResponse.getArticlesBySectionId.filter(
+												(article) => article.SECTION === this.section.sectionId
+											);
+										this.articles.sort((n1, n2) => {
+											if (n1.ORDER > n2.ORDER) {
+												return 1;
+											}
 
-                  if (n1.ORDER < n2.ORDER) {
-                    return -1;
-                  }
+											if (n1.ORDER < n2.ORDER) {
+												return -1;
+											}
 
-                  return 0;
-                });
-                this.isLoading = false;
-              }, (articlesError: any) => {
-                console.log(articlesError);
-              }
-            );
-          }, (categoryError: any) => {
-            console.log(categoryError);
-          }
-        );
-      }, (sectionsError: any) => {
-        console.log(sectionsError);
-      }
-    );
-  }
+											return 0;
+										});
+										this.isLoading = false;
+									},
+									(articlesError: any) => {
+										console.log(articlesError);
+									}
+								);
+						},
+						(categoryError: any) => {
+							console.log(categoryError);
+						}
+					);
+			},
+			(sectionsError: any) => {
+				console.log(sectionsError);
+			}
+		);
+	}
 
-  public ngOnInit() {
+	public ngOnInit() {}
 
-  }
+	private convertCategory(categoryObj): Category {
+		const category: Category = new Category(
+			categoryObj['DATA'].name,
+			categoryObj['DATA'].sourceLanguage,
+			categoryObj['DATA'].isDraft,
+			categoryObj['DATA'].order,
+			categoryObj['DATA'].description,
+			categoryObj['DATA'].categoryId
+		);
+		return category;
+	}
 
-  private convertCategory(categoryObj): Category {
-    const category: Category = new Category(
-      categoryObj.NAME,
-      categoryObj.SOURCE_LANGUAGE,
-      categoryObj.IS_DRAFT,
-      categoryObj.ORDER,
-      categoryObj.DESCRIPTION,
-      categoryObj.CATEGORY_ID,
-    );
-    return category;
-  }
+	private convertSection(sectionObj): Section {
+		const section: Section = new Section(
+			sectionObj['DATA'].name,
+			sectionObj['DATA'].sourceLanguage,
+			sectionObj['DATA'].category['categoryId'],
+			sectionObj['DATA'].sortBy,
+			sectionObj['DATA'].order,
+			sectionObj['DATA'].description,
+			sectionObj['DATA'].sectionId
+		);
+		return section;
+	}
 
-  private convertSection(sectionObj): Section {
-    const section: Section = new Section(
-      sectionObj.NAME,
-      sectionObj.SOURCE_LANGUAGE,
-      sectionObj.CATEGORY,
-      sectionObj.SORT_BY,
-      sectionObj.ORDER,
-      sectionObj.DESCRIPTION,
-      sectionObj.SECTION_ID
-    );
-    return section;
-  }
+	public drop(event: CdkDragDrop<string[]>) {
+		moveItemInArray(this.articles, event.previousIndex, event.currentIndex);
+		this.reorderContent();
+	}
 
-  public drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.articles, event.previousIndex, event.currentIndex);
-    this.reorderContent();
-  }
+	public sendToTop(index, array) {
+		moveItemInArray(array, index, 0);
+		this.reorderContent();
+	}
 
-  public sendToTop(index, array) {
-    moveItemInArray(array, index, 0);
-    this.reorderContent();
-  }
-
-  public reorderContent() {
-    const reorderBody = [];
-    this.articles.forEach((article, index) => {
-      reorderBody.push({ ID: article.ARTICLE_ID, ORDER: index + 1 });
-      if (index === this.articles.length - 1) {
-        this.guideService.reorderContent('articles', reorderBody, this.sectionId).subscribe(
-          (reorderResponse: any) => {
-            // do nothing
-          }, (reorderError: any) => {
-            this.errorMessage = reorderError.error.ERROR;
-          }
-        );
-      }
-    });
-  }
-
+	public reorderContent() {
+		const reorderBody = [];
+		this.articles.forEach((article, index) => {
+			reorderBody.push({ ID: article.ARTICLE_ID, ORDER: index + 1 });
+			if (index === this.articles.length - 1) {
+				this.guideService
+					.reorderContent('articles', reorderBody, this.section.sectionId)
+					.subscribe(
+						(reorderResponse: any) => {
+							// do nothing
+						},
+						(reorderError: any) => {
+							this.errorMessage = reorderError.error.ERROR;
+						}
+					);
+			}
+		});
+	}
 }
--- ngDesk-UI/src/app/guide/arrange/arrange-sections/arrange-sections.component.ts
+++ ngDesk-UI/src/app/guide/arrange/arrange-sections/arrange-sections.component.ts
@@ -6,112 +6,121 @@ import { Category } from '../../../models/category';
 import { GuideService } from '../../guide.service';
 
 @Component({
-  selector: 'app-arrange-sections',
-  templateUrl: './arrange-sections.component.html',
-  styleUrls: ['../../guide.component.scss']
+	selector: 'app-arrange-sections',
+	templateUrl: './arrange-sections.component.html',
+	styleUrls: ['../../guide.component.scss'],
 })
 export class ArrangeSectionsComponent implements OnInit {
+	public category: Category = new Category('', '', false, 0);
+	public sections = [];
+	public categoryId: string;
+	public errorMessage: string;
+	public isLoading = true;
 
-  public category: Category = new Category('', '', false, 0);
-  public sections = [];
-  public categoryId: string;
-  public errorMessage: string;
-  public isLoading = true;
+	constructor(
+		private route: ActivatedRoute,
+		private guideService: GuideService
+	) {
+		this.categoryId = this.route.snapshot.params['categoryId'];
+		// get category
+		this.guideService.getKbCategoryById(this.categoryId).subscribe(
+			(categoryResponse: any) => {
+				this.category = this.convertCategory(categoryResponse);
+				// get all sections in this category
+				this.guideService.getKbSectionByCategoryId(this.categoryId).subscribe(
+					(sectionsResponse: any) => {
+						sectionsResponse.DATA.forEach((section, index) => {
+							console.log('sectionsResponse', sectionsResponse);
+							this.guideService
+								.getArticlesBySectionId(section.sectionId)
+								.subscribe(
+									(articlesResponse: any) => {
+										console.log('articlesResponse', articlesResponse);
+										// for each section, add its articles
+										section['ARTICLES'] =
+											articlesResponse.getArticlesBySectionId.filter(
+												(article) => article.SECTION === section.sectionId
+											);
+										this.sections.push(section);
+										// waits until all sections with articles are loaded before sorting
+										if (sectionsResponse.DATA.length === this.sections.length) {
+											this.sections.sort((n1, n2) => {
+												if (n1.ORDER > n2.ORDER) {
+													return 1;
+												}
 
-  constructor(
-    private route: ActivatedRoute,
-    private guideService: GuideService,
-  ) {
-    this.categoryId = this.route.snapshot.params['categoryId'];
-    // get category
-    this.guideService.getCategoryById(this.categoryId).subscribe(
-      (categoryResponse: any) => {
-        this.category = this.convertCategory(categoryResponse);
-        // get all sections in this category
-        this.guideService.getSections(this.categoryId).subscribe(
-          (sectionsResponse: any) => {
-            sectionsResponse.DATA.forEach((section, index) => {
-              this.guideService.getArticlesBySection(section.SECTION_ID, false).subscribe(
-                (articlesResponse: any) => {
-                  // for each section, add its articles
-                  section['ARTICLES'] = articlesResponse.DATA.filter(article => article.SECTION === section.SECTION_ID);
-                  this.sections.push(section);
-                  // waits until all sections with articles are loaded before sorting
-                  if (sectionsResponse.DATA.length === this.sections.length) {
-                    this.sections.sort((n1, n2) => {
-                      if (n1.ORDER > n2.ORDER) {
-                        return 1;
-                      }
+												if (n1.ORDER < n2.ORDER) {
+													return -1;
+												}
 
-                      if (n1.ORDER < n2.ORDER) {
-                        return -1;
-                      }
+												return 0;
+											});
+											this.isLoading = false;
+										}
+									},
+									(articlesError: any) => {
+										console.log(articlesError);
+										this.isLoading = false;
+									}
+								);
+						});
+						if (sectionsResponse.DATA.length === 0) {
+							this.isLoading = false;
+						}
+					},
+					(sectionsError: any) => {
+						console.log(sectionsError);
+						this.isLoading = false;
+					}
+				);
+			},
+			(categoryError: any) => {
+				console.log(categoryError);
+				this.isLoading = false;
+			}
+		);
+	}
 
-                      return 0;
-                    });
-                    this.isLoading = false;
-                  }
-                }, (articlesError: any) => {
-                  console.log(articlesError);
-                  this.isLoading = false;
-                }
-              );
-            });
-            if (sectionsResponse.DATA.length === 0) {
-              this.isLoading = false;
-            }
-          }, (sectionsError: any) => {
-            console.log(sectionsError);
-            this.isLoading = false;
-          }
-        );
-      }, (categoryError: any) => {
-        console.log(categoryError);
-        this.isLoading = false;
-      }
-    );
-  }
+	public ngOnInit() {}
 
-  public ngOnInit() {
+	private convertCategory(categoryObj): Category {
+		const category: Category = new Category(
+			categoryObj['DATA'].name,
+			categoryObj['DATA'].sourceLanguage,
+			categoryObj['DATA'].isDraft,
+			categoryObj['DATA'].order,
+			categoryObj['DATA'].description,
+			categoryObj['DATA'].categoryId
+		);
+		return category;
+	}
 
-  }
+	public drop(event: CdkDragDrop<string[]>) {
+		moveItemInArray(this.sections, event.previousIndex, event.currentIndex);
+		this.reorderContent();
+	}
 
-  private convertCategory(categoryObj): Category {
-    const category: Category = new Category(
-      categoryObj.NAME,
-      categoryObj.SOURCE_LANGUAGE,
-      categoryObj.IS_DRAFT,
-      categoryObj.ORDER,
-      categoryObj.DESCRIPTION,
-      categoryObj.CATEGORY_ID,
-    );
-    return category;
-  }
+	public sendToTop(index, array) {
+		moveItemInArray(array, index, 0);
+		this.reorderContent();
+	}
 
-  public drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.sections, event.previousIndex, event.currentIndex);
-    this.reorderContent();
-  }
-
-  public sendToTop(index, array) {
-    moveItemInArray(array, index, 0);
-    this.reorderContent();
-  }
-
-  public reorderContent() {
-    const reorderBody = [];
-    this.sections.forEach((section, index) => {
-      reorderBody.push({ ID: section.SECTION_ID, ORDER: index + 1 });
-      if (index === this.sections.length - 1) {
-        this.guideService.reorderContent('sections', reorderBody, this.categoryId).subscribe(
-          (reorderResponse: any) => {
-            // do nothing
-          }, (reorderError: any) => {
-            this.errorMessage = reorderError.error.ERROR;
-          }
-        );
-      }
-    });
-  }
-
+	public reorderContent() {
+		const reorderBody = [];
+		this.sections.forEach((section, index) => {
+			reorderBody.push({ ID: section.sectionId, ORDER: index + 1 });
+			if (index === this.sections.length - 1) {
+				this.guideService
+					.reorderContent('sections', reorderBody, this.category.categoryId)
+					.subscribe(
+						(reorderResponse: any) => {
+							// do nothing
+						},
+						(reorderError: any) => {
+							this.errorMessage = reorderError.error.ERROR;
+						}
+					);
+			}
+		});
+	}
 }
