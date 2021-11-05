0900e3246069d01f06f372aa575e0ff84cbd669a
Sharath Satish <sharath.satish@allbluesolutions.com>
2021-11-05 15:05:34 +0530

    Update MemoryStatusAPI.java

--- ngDesk-Data-Service/src/main/java/com/ngdesk/data/dao/MemoryStatusAPI.java
+++ ngDesk-Data-Service/src/main/java/com/ngdesk/data/dao/MemoryStatusAPI.java
@@ -1,5 +1,9 @@
 package com.ngdesk.data.dao;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +24,12 @@ public class MemoryStatusAPI {
 		System.out.println("Used Memory: " + (instance.totalMemory() - instance.freeMemory()) / mb);
 		// Maximum available memory
 		System.out.println("Max Memory: " + instance.maxMemory() / mb);
+		
+		RuntimeMXBean RuntimemxBean = ManagementFactory.getRuntimeMXBean();
+		List<String> args2=RuntimemxBean.getInputArguments();
+
+		for(int i=0;i<args2.size();i++) {
+		    System.out.println(args2.get(i)); 
+		}
 	}
 }
