0a1d776908f444b1f318c5021cb54ca25dd1fff8
vmirji <vishal.mirji@allbluesolutions.com>
2021-11-09 15:35:58 +0530

    Comment Message Validation

--- ngDesk-KnowledgeBase-Service/src/main/java/com/ngdesk/knowledgebase/article/dao/CommentMessage.java
+++ ngDesk-KnowledgeBase-Service/src/main/java/com/ngdesk/knowledgebase/article/dao/CommentMessage.java
@@ -5,27 +5,29 @@ import java.util.Date;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ngdesk.commons.annotations.CustomNotEmpty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public class CommentMessage {
-	
+
 	@Schema(description = "message id")
 	private String messageId;
-	
+
 	@Schema(description = "message")
+	@CustomNotEmpty(message = "DAO_VARIABLE_REQUIRED", values = { "MESSAGE" })
 	private String message;
-	
+
 	@Schema(description = "sender")
 	private String sender;
-	
+
 	@Schema(description = "Date created")
 	private Date dateCreated;
-	
+
 	public CommentMessage() {
-		
+
 	}
-	
+
 	public CommentMessage(String messageId, String message, String sender, Date dateCreated) {
 		super();
 		this.messageId = messageId;
@@ -37,27 +39,33 @@ public class CommentMessage {
 	public String getMessageId() {
 		return messageId;
 	}
+
 	public void setMessageId(String messageId) {
 		this.messageId = messageId;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
 	public String getSender() {
 		return sender;
 	}
+
 	public void setSender(String sender) {
 		this.sender = sender;
 	}
+
 	public Date getDateCreated() {
 		return dateCreated;
 	}
+
 	public void setDateCreated(Date dateCreated) {
 		this.dateCreated = dateCreated;
 	}
-	
-	
+
 }
